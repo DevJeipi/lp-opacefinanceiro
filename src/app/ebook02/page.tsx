4a67a51bda24dcab766f22295c3ec6ff331545e5
DevJeipi <joaopedroghilardi@gmail.com.br>
2025-06-07 15:06:04 -0300

    type error fix

--- src/app/ebook02/page.tsx
+++ src/app/ebook02/page.tsx
@@ -24,7 +24,7 @@ import {
 } from 'lucide-react'
 
 export default function Ebook02() {
-    export const cards = [
+    const cards = [
         {
             title: 'Sonhadores presos no vermelho',
             description:
