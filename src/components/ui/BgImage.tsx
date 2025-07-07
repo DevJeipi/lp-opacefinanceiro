99f86fdd5e2ef797d5473f5ea2df0d1fc2cda3b7
DevJeipi <joaopedroghilardi@gmail.com.br>
2025-07-07 16:43:14 -0300

    bg hero image changed to html img tag

--- src/components/ui/BgImage.tsx
+++ src/components/ui/BgImage.tsx
@@ -2,7 +2,12 @@ export function BgImage() {
     return (
         <>
             <div className="absolute inset-0 -z-20">
-                <img src="/imagemdefundologin.webp" alt="Bg fundo" />
+                <img
+                    src="/imagemdefundologin.webp"
+                    alt="Gráfico de fundo"
+                    className="h-full w-full object-cover"
+                    sizes="(max-width: 768px) 100vw, 50vw"
+                />
             </div>
             <div className="bg-primary-blue/40 absolute inset-0 -z-10" />
         </>
