dec64750da51fde21f57d8bb64af6771094afae6
DevJeipi <joaopedroghilardi@gmail.com.br>
2025-07-14 16:07:55 -0300

    hotjar add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Metadata } from 'next'
 import { Lato } from 'next/font/google'
+import Script from 'next/script'
 
 // Metadata for SEO
 export const metadata: Metadata = {
@@ -70,6 +71,20 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="pt-br" dir="ltr">
+            <head>
+                <Script id="hotjar-init" strategy="afterInteractive">
+                    {`
+             (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:6463913,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          `}
+                </Script>
+            </head>
             <body className={`${lato.className} antialiased`}>{children}</body>
         </html>
     )
